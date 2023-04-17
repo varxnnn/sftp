475d48f52aee318235a3317e73b22a657613a141
Varun Sharma <varunvgs2000@gmail.com>
2023-04-17 12:54:00 +0530

    more settings

--- script.sh
+++ script.sh
@@ -7,6 +7,9 @@ cd /home/runner/work/sftp/sftp
 lftp<<END_SCRIPT
 set sftp:auto-confirm yes
 set ssl:verify-certificate no
+set ftp:use-allo false
+set ftp:passive-mode true
+set ftp:prefer-epsv false
 open sftp://$HOST
 user $USER $PASSWD
 mput -d build/*
