ee6fd0fb7a3335b7aeb3c9aefd18ea7b79ec1249
Varun Sharma <varunvgs2000@gmail.com>
2023-04-17 12:51:26 +0530

    ls

--- script.sh
+++ script.sh
@@ -2,14 +2,13 @@ HOST=45.142.237.166:9090
 USER=testuser
 PASSWD=testpassword
 
-cd /home/testuser/build
+cd /home/runner/work/sftp/sftp
 
 lftp<<END_SCRIPT
 set sftp:auto-confirm yes
 set ssl:verify-certificate no
 open sftp://$HOST
 user $USER $PASSWD
-
 mput -d build/*
 bye
 END_SCRIPT
