e66206e0e1e66bd0b67ea80b28a444c77dc0b7fa
Varun Sharma <varunvgs2000@gmail.com>
2023-04-18 12:05:48 +0530

    root user

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 HOST=45.142.237.166:9090
-USER=testuser
-PASSWD=testpassword
+USER=root
+TARGET=/home/testuser/build
 
 cd /home/runner/work/sftp/sftp
 ls -l build
@@ -14,8 +14,7 @@ set ssl:verify-certificate no
 set ftp:use-allo false
 set ftp:passive-mode true
 set ftp:prefer-epsv false
-open sftp://$HOST
-user $USER $PASSWD
-mput -d build/*
+open sftp://$ROOT@$HOST
+mput -d $TARGET build/* 
 bye
 END_SCRIPT
